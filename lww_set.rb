7aa62597f22bc1358f944265cec379adce06d656
Thomas Pun <thomas@pun.io>
2018-04-22 20:48:23 +0800

    implement #set

--- lww_set.rb
+++ lww_set.rb
@@ -9,17 +9,33 @@ class LWWSet
     @remove_set = Set.new
   end
 
-  def add(data, timestamp=Time.now)
+  def add(data, timestamp=Time.now.to_i)
     element = TimestampedElement.new(data, timestamp)
     @add_set.add(element)
   end
 
-  def remove(data, timestamp=Time.now)
+  def remove(data, timestamp=Time.now.to_i)
     element = TimestampedElement.new(data, timestamp)
     @remove_set.add(element)
   end
 
-  def set(at=Time.now)
-    return Set.new
+  def set
+    final_elements = @add_set.select do |added_element|
+      !remove_later?(added_element)
+     end
+    Set.new(final_elements.map(&:data))
+  end
+
+  private
+  def remove_later?(added_element)
+    # if the removal is recorded at the exact same time
+    # as a addition, preference is given to addition, i.e.,
+    # an element is only deletered if there is a removal
+    # AFTER the addition's time
+    removal_record = @remove_set.find do |removed_element|
+      removed_element.data == added_element.data &&
+      removed_element.timestamp > added_element.timestamp
+    end
+    removal_record != nil
   end
 end
--- lww_set_test.rb
+++ lww_set_test.rb
@@ -5,19 +5,19 @@ describe 'LWWSet' do
   before(:each) do
     @lww = LWWSet.new
     @data = 'blah'
-    @timestamp = 10000
+    @timestamp = Time.now -  1000
   end
   describe '#add' do
     context 'when adding an element' do
       it 'adds to add_set' do
         @lww.add(@data, @timestamp)
-        @lww.add_set.should have(1).items
+        expect(@lww.add_set.size).to be(1)
       end
 
       it 'is idempotent' do
         @lww.add(@data, @timestamp)
         @lww.add(@data, @timestamp)
-        @lww.add_set.should have(1).items
+        expect(@lww.add_set.size).to be(1)
       end
     end
   end
@@ -26,15 +26,41 @@ describe 'LWWSet' do
     context 'when removing an element' do
       it 'adds element to remove_set' do
         @lww.remove(@data, @timestamp)
-        @lww.remove_set.should have(1).items
+        expect(@lww.remove_set.size).to be(1)
       end
     end
   end
 
   describe '#set' do
-    it 'removes previously added element only if removal happens after'
-    it 'adds previously removed element only if addition happens after'
-    it 'gives bias to addition'
-    it 'returns a set'
+    it 'removes previously added element only if removal happens after' do
+      @lww.add(@data, @timestamp)
+      another_data = 'nah'
+      @lww.add(another_data, @timestamp+10)
+      expect(@lww.set).to contain_exactly(@data, another_data)
+
+      @lww.remove(@data, @timestamp+20)
+      expect(@lww.set).to contain_exactly(another_data)
+    end
+
+    it 'adds previously removed element only if addition happens after' do
+      @lww.add(@data, @timestamp)
+      @lww.remove(@data, @timestamp+10)
+      expect(@lww.set.size).to be(0)
+
+      another_data = 'nah'
+      @lww.add(another_data, @timestamp+20)
+      @lww.add(@data, @timestamp+30)
+      expect(@lww.set).to contain_exactly(@data, 'nah')
+    end
+
+    it 'gives bias to addition' do
+      @lww.add(@data, @timestamp)
+      @lww.remove(@data, @timestamp)
+      expect(@lww.set).to contain_exactly(@data)
+    end
+
+    it 'returns a set' do
+      expect(@lww.set.class).to be(Set)
+    end
   end
 end
