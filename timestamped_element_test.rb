668312ae2fa036934ad33e01342524bb63834f4b
Thomas Pun <thomas@pun.io>
2018-04-22 20:54:12 +0800

    test if same data but different timestamp

--- timestamped_element_test.rb
+++ timestamped_element_test.rb
@@ -10,22 +10,38 @@ describe 'TimestampedElement' do
   end
 
   context 'when inserting into a set' do
-    it 'object with the same data and timestamp is considered identical' do
+    it 'objects with the same data and timestamp are considered identical' do
       @another = TimestampedElement.new(@element.data, @element.timestamp)
       set = Set.new
       set.add(@element)
       set.add(@another)
       expect(set.size).to be(1)
     end
+
+    it 'objects with the same data but different timestamp are considered different' do
+      @another = TimestampedElement.new(@element.data, @element.timestamp+1000)
+      set = Set.new
+      set.add(@element)
+      set.add(@another)
+      expect(set.size).to be(2)
+    end
   end
 
   context 'when deleting from a set' do
-    it 'object with the same data and timestamp is considered identical' do
+    it 'objects with the same data and timestamp are considered identical' do
       @another = TimestampedElement.new(@element.data, @element.timestamp)
       set = Set.new
       set.add(@element)
       set.delete(@another)
       expect(set.size).to be(0)
+    end
+
+    it 'objects with the same data but different timestamp are considered different' do
+      @another = TimestampedElement.new(@element.data, @element.timestamp+1000)
+      set = Set.new
+      set.add(@element)
+      set.delete(@another)
+      expect(set.size).to be(1)
     end
   end
 end
